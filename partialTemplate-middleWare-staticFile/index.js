848cbe8af299f14091cea5e7da9cf499ad6582a0
prashant <pra98935@gmail.com>
2018-11-28 19:08:28 +0530

    middleware

--- partialTemplate-middleWare-staticFile/index.js
+++ partialTemplate-middleWare-staticFile/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 
+app.use('/assets', express.static('assets'));
+// app.use('/assets1', express.static('assets')); // this code also work no need to folder name same at first place
+
 app.get('/', function(req, res){
     res.render('home')
 })
