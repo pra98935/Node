7d96b1062021df4691ad87ae3adcacc25dd665b5
prashant <pra98935@gmail.com>
2018-12-26 01:34:02 +0530

    changes

--- crud/index.js
+++ crud/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
+var bodyParser = require('body-parser');
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', function(req, res){
     // res.render('login');
@@ -10,6 +14,23 @@ app.get('/', function(req, res){
 
 app.get('/signup', function(req, res){
     res.render('signup');
+});
+
+app.post('/signup', urlencodedParser, function(req, res){
+    userInfo = {
+        'name':req.body.name,
+        'country':req.body.country,
+        'state':req.body.state,
+        'hobby':req.body.hobby,
+        'gender':req.body.gender,
+        'profile':req.body.profile,
+        'email':req.body.email,
+        'pswd':req.body.pswd,
+        'address':req.body.address,
+        'remember':req.body.remember
+    }
+    console.log(userInfo);
+    res.render('signup.ejs', {data:userInfo});
 });
 
 app.get('/resetpassword', function(req, res){
